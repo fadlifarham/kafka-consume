d4d5f79294f1a7ef651f3247d19878020cecf950
fadli7 <fadlifarham10@gmail.com>
2020-01-19 11:48:56 +0700

    update kafka producer

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ KEY = "snort"
 
 def publish_message(producer_instance, topic_name, key, value):
     try:
-        print(value)
+        # print(value)
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
